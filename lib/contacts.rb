b5987628350b3519fa7a94216e77776b05a472a8
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-12 17:57:44 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -15,5 +15,20 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
+  
+contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        date.delete_if {|ice_cream| ice_cream == "strawberry"}
+
 
 end
+
+def remove_strawberry(contacts)
+  freddy = contacts["Freddy Mercury"]
+  freddy.each do |attribute, value|
+    if attribute == :favorite_ice_cream_flavors
+      value.delete_if{|ice_cream| ice_cream == "strawberry"}
+    end
+    contacts
+  end
